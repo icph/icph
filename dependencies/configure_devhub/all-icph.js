e5fdb4bad87bc1108174dc034a94ee8c5d91ba34
Nick Blair <nicholas.h.blair@intel.com>
2017-07-10 13:53:09 -0400

    Add all-icph.js Add additional configuration file for default homepage

---
+++ dependencies/configure_devhub/all-icph.js
@@ -0,0 +1,3 @@
+pref("browser.startup.homepage",           "data:text/plain,browser.startup.homepage=http://localhost");
+pref("startup.homepage_override_url",       "http://localhost");
+pref("startup.homepage_welcome_url",        "http://localhost");
